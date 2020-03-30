d1708d76ec045fe691bfbb331e3d405833814648
Memis Cetinkaya <memiscet@gmail.com>
2020-03-29 22:01:11 -0500

    first commit for java-sandbox

--- ByteShortIntLong/src/acamdemy/learnprogramming/Main.java
+++ ByteShortIntLong/src/acamdemy/learnprogramming/Main.java
@@ -6,12 +6,12 @@ public class Main {
         int myValue= 10000;
         int myMinIntValue = Integer.MIN_VALUE;
         int myMaxValue = Integer.MAX_VALUE;
-
+        int myInt=2_147_483_647;
         System.out.println("Integer Minimum Value ="+myMinIntValue);
         System.out.println("Integer Maximum Value ="+myMaxValue);
 
         System.out.println("Integer Busted Minimum Value ="+( myMinIntValue-1));
         System.out.println("Integer Busted Maximum Value ="+(myMaxValue+1));
-
+        System.out.format( "myInt is %d ",myInt );
     }
 }
